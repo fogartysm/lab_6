772c56eb0c5bf833d7138e6b05666103da4d354d
sfogarty <fogarty.s@ufl.edu>
2022-03-14 21:55:48 -0400

    adding 2 random numbers

---
+++ Lab06/src/Lab06.java
@@ -0,0 +1,23 @@
+/**********************************
+ *  Written by: Shawn Fogarty     *
+ *  Written on: 14 Mar 2022       *
+ *  Class: COP3502C               *
+ *  Professor: Amanpreet Kapoor   *
+ *  Purpose: Lab 6 Software Eng.  *
+ **********************************/
+
+import java.util.Random;
+
+public class Lab06
+{
+    public static void main(String[] args)
+    {
+        //generating 2 random numbers and adding them together
+        Random random = new Random();
+        int a = random.nextInt(100);
+        int b = random.nextInt(100);
+
+        System.out.println(a + " + " + b + " = " + (a + b));
+
+    }
+}
